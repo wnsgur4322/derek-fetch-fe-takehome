0781de8773ea86e671ef73105d8c162e11178610
Derek Jeong <wnsgur3470@gmail.com>
2025-01-30 00:47:51 -0800

    add location search

--- my-app/app/search/page.tsx
+++ my-app/app/search/page.tsx
@@ -15,9 +15,12 @@ export default function Search() {
   const [pageSize, setPageSize] = useState(25); // page size
   const [sortField, setSortField] = useState<"breed" | "name" | "age">("breed"); // default sorting field
   const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc"); // default sorting order
-  const [zipCode, setZipCode] = useState("");
   const [ageMin, setAgeMin] = useState<number | undefined>(undefined);
   const [ageMax, setAgeMax] = useState<number | undefined>(undefined);
+
+  const [city, setCity] = useState(""); // New state for city
+  const [state, setState] = useState(""); // New state for state
+  const [zipCodes, setZipCodes] = useState<string[]>([]); // Zip codes fetched based on city and state
 
   const router = useRouter();
 
@@ -39,7 +42,35 @@ export default function Search() {
     fetchBreeds();
   }, []);
 
-  const fetchDogs = async (breed = "", zipCode = "", ageMin?: number, ageMax?: number, page = 1, pageSize = 25, sortField = "breed", sort = "asc") => {
+  const fetchZipCodes = async () => {
+    if (!city && !state) {
+      setZipCodes([]);
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        "https://frontend-take-home-service.fetch.com/locations/search",
+        {
+          city,
+          states: state ? [state] : undefined,
+        },
+        { withCredentials: true }
+      );
+      const fetchedZipCodes = response.data.results.map(
+        (location: { zip_code: string }) => location.zip_code
+      );
+      setZipCodes(fetchedZipCodes);
+    } catch (error) {
+      console.error("Failed to fetch zip codes", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchZipCodes();
+  }, [city, state]);
+
+  const fetchDogs = async (breed = "", page = 1, pageSize = 25) => {
     try {
       // fetch dog IDs from the search API
       const from = (page - 1) * pageSize; // calculate the starting index
@@ -47,9 +78,11 @@ export default function Search() {
       // create query parameters
       let queryParams = `size=${pageSize}&from=${from}&sort=${sortField}:${sortOrder}`;
       if (breed) queryParams += `&breeds=${breed}`;
-      if (zipCode) queryParams += `&zipCodes=${zipCode}`;
       if (ageMin !== undefined) queryParams += `&ageMin=${ageMin}`;
       if (ageMax !== undefined) queryParams += `&ageMax=${ageMax}`;
+      if (zipCodes.length > 0) queryParams += `&zipCodes=${zipCodes.join(",")}`;
+
+      console.log("Query Params", queryParams);
 
       // fetching dog ids
       const searchResponse = await axios.get(
@@ -59,6 +92,7 @@ export default function Search() {
   
       const dogIds = searchResponse.data.resultIds as string[];
       setTotal(searchResponse.data.total); // get total number of results
+      console.log("Dog IDs", dogIds);
   
       // fetch detailed dog data using the dog IDs
       if (dogIds.length > 0) {
@@ -78,8 +112,8 @@ export default function Search() {
   
 
   useEffect(() => {
-    fetchDogs(selectedBreed, zipCode, ageMin, ageMax, page, pageSize, sortField, sortOrder);
-  }, [selectedBreed, zipCode, ageMin, ageMax, page, pageSize, sortField, sortOrder]);
+    fetchDogs(selectedBreed, page, pageSize);
+  }, []);
 
   const toggleFavorite = (dog: Dog) => {
     setFavorites((prev) =>
@@ -108,71 +142,120 @@ export default function Search() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Search Dogs</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
-        <div className="flex flex-col md:flex-row gap-4 mb-2 items-center bg-background p-4 rounded-md shadow-md">
-          <select
-            value={selectedBreed}
-            onChange={(e) => setSelectedBreed(e.target.value)}
-            className="p-2 rounded border text-black focus:ring focus:ring-blue-500"
-          >
-            <option value="">All Breeds</option>
-            {breeds.map((breed) => (
-              <option key={breed} value={breed}>
-                {breed}
-              </option>
-            ))}
-          </select>
-          <select
-            value={sortField}
-            onChange={(e) => setSortField(e.target.value as "breed" | "name" | "age")}
-            className="p-2 rounded border text-black focus:ring focus:ring-blue-500"
-          >
-            <option value="breed">Sort by Breed</option>
-            <option value="name">Sort by Name</option>
-            <option value="age">Sort by Age</option>
-          </select>
-          <select
-            value={sortOrder}
-            onChange={(e) => setSortOrder(e.target.value as "asc" | "desc")}
-            className="p-2 rounded border text-black focus:ring focus:ring-blue-500"
-          >
-            <option value="asc">Sort: A-Z</option>
-            <option value="desc">Sort: Z-A</option>
-          </select>
+        <div className="flex flex-wrap md:flex-row gap-4 mb-2 items-center bg-background p-4 rounded-md shadow-md">
+          <div className="flex flex-col">
+            <label className="text-sm font-medium mb-1" htmlFor="breed">
+              Breed
+            </label>
+            <select
+              value={selectedBreed}
+              onChange={(e) => setSelectedBreed(e.target.value)}
+              className="p-2 rounded border text-black focus:ring focus:ring-blue-500"
+            >
+              <option value="">All Breeds</option>
+              {breeds.map((breed) => (
+                <option key={breed} value={breed}>
+                  {breed}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="flex flex-col">
+            <label className="text-sm font-medium mb-1" htmlFor="sortField">
+            Sort By
+            </label>
+            <select
+              value={sortField}
+              onChange={(e) => setSortField(e.target.value as "breed" | "name" | "age")}
+              className="p-2 rounded border text-black focus:ring focus:ring-blue-500"
+            >
+              <option value="breed">Breed</option>
+              <option value="name">Name</option>
+              <option value="age">Age</option>
+            </select>
+          </div>
+          <div className="flex flex-col">
+            <label className="text-sm font-medium mb-1" htmlFor="sortOrder">
+              Sort Order
+            </label>
+            <select
+              value={sortOrder}
+              onChange={(e) => setSortOrder(e.target.value as "asc" | "desc")}
+              className="p-2 rounded border text-black focus:ring focus:ring-blue-500"
+            >
+              <option value="asc">Sort: A-Z</option>
+              <option value="desc">Sort: Z-A</option>
+            </select>
+          </div>
+          <div className="flex flex-col">
+            <label className="text-sm font-medium mb-1" htmlFor="city">
+              City
+            </label>
+            <input
+              type="text"
+              placeholder="City"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              className="p-2 rounded border text-black focus:ring focus:ring-blue-500 placeholder-gray-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-sm font-medium mb-1" htmlFor="state">
+              State
+            </label>
+            <input
+              type="text"
+              placeholder="State (e.g., CA)"
+              value={state}
+              onChange={(e) => setState(e.target.value)}
+              className="p-2 rounded border text-black focus:ring focus:ring-blue-500 placeholder-gray-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-sm font-medium mb-1" htmlFor="minAge">
+              Min Age
+            </label>
+            <input
+              type="number"
+              placeholder="Min Age"
+              min={0}
+              value={ageMin !== undefined ? ageMin : ""}
+              onChange={(e) => {
+                const value = e.target.value;
+                setAgeMin(value === "" ? undefined : Number(value));
+              }}
+              className="p-2 rounded border text-black focus:ring focus:ring-blue-500 appearance-none placeholder-gray-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-sm font-medium mb-1" htmlFor="maxAge">
+              Max Age
+            </label>
+            <input
+              type="number"
+              placeholder="Max Age"
+              min={0}
+              value={ageMax !== undefined ? ageMax : ""}
+              onChange={(e) => {
+                const value = e.target.value;
+                setAgeMax(value === "" ? undefined : Number(value));
+              }}
+              className="p-2 rounded border text-black focus:ring focus:ring-blue-500 appearance-none placeholder-gray-500"
+            />
+          </div>
+          {/* Search Button */}
+          <div className="flex flex-col justify-end">
+          <label className="text-sm font-medium mb-1 text-background" htmlFor="maxAge">
+              Search
+            </label>
+            <button
+              onClick={() => fetchDogs(selectedBreed, page, pageSize)}
+              className="bg-highlight text-white py-2 px-4 rounded-md shadow-md hover:bg-highlight hover:opacity-90 focus:outline-none focus:ring focus:ring-blue-500"
+            >
+              Search
+            </button>
+          </div>
         </div>
-        <div className="flex flex-col md:flex-row gap-4 mb-2 items-center bg-background p-4 rounded-md shadow-md">
-          <input
-            type="text"
-            placeholder="Zip Code"
-            value={zipCode}
-            onChange={(e) => {
-              const value = e.target.value;
-              if (/^\d*$/.test(value)) setZipCode(value); // Allow only numbers
-            }}
-            className="p-2 rounded border text-black focus:ring focus:ring-blue-500 placeholder-gray-500"
-          />
-          <input
-            type="text"
-            placeholder="Min Age"
-            value={ageMin}
-            onChange={(e) => {
-              const value = e.target.value;
-              if (/^\d*$/.test(value)) setAgeMin(Number(value)); // Allow only numbers
-            }}
-            className="p-2 rounded border text-black focus:ring focus:ring-blue-500 appearance-none placeholder-gray-500"
-          />
-          <input
-            type="text"
-            placeholder="Max Age"
-            value={ageMax}
-            onChange={(e) => {
-              const value = e.target.value;
-              if (/^\d*$/.test(value)) setAgeMax(Number(value)); // Allow only numbers
-            }}
-            className="p-2 rounded border text-black focus:ring focus:ring-blue-500 appearance-none placeholder-gray-500"
-          />
-        </div>
-      </div>
       <div className="flex items-center justify-between mb-4">
         <p className="text-sm">
           Showing {pageSize} results per page (Total: {total} results)
@@ -230,9 +313,9 @@ export default function Search() {
       <div className="mt-4 flex justify-between items-center">
         <button
           onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
-          disabled={page === 1}
+          disabled={page === 1 || totalPages === 0}
           className={`px-4 py-2 rounded ${
-            page === 1 ? "bg-gray-400 cursor-not-allowed" : "bg-highlight text-white hover:opacity-90"
+            page === 1 || totalPages === 0 ? "bg-gray-400 cursor-not-allowed" : "bg-highlight text-white hover:opacity-90"
           }`}
         >
           Previous
@@ -242,9 +325,9 @@ export default function Search() {
         </p>
         <button
           onClick={() => setPage((prev) => Math.min(prev + 1, totalPages))}
-          disabled={page === totalPages}
+          disabled={page === totalPages || totalPages === 0}
           className={`px-4 py-2 rounded ${
-            page === totalPages
+            page === totalPages || totalPages === 0
               ? "bg-gray-400 cursor-not-allowed"
               : "bg-highlight text-white hover:opacity-90"
           }`}
